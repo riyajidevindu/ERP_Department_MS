aac398f1f4623bdf66c3ec9aeb801e9a7d1c5d14
riyajidevindu <riyajidevindu@gmail.com>
2024-03-25 01:25:15 +0530

    Graduate Controllers add

--- GraduateManagement/ERP.GraduateManagement.Api/Program.cs
+++ GraduateManagement/ERP.GraduateManagement.Api/Program.cs
@@ -6,6 +6,8 @@ using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 //Get Connection String
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -16,6 +18,8 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connect
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
